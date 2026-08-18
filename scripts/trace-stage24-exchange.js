dc2dcd99977e899b9537ae00a64242326e5b6a44
swenstock <swenstock@gmail.com>
2026-08-18 07:40:23 -0600

    Stage 24: remove temporary exchange trace

--- scripts/trace-stage24-exchange.js
+++
@@ -1,4 +0,0 @@
-const { exactV45Shell } = require('../server/v45ExactShell');
-const s=exactV45Shell.toString('utf8');
-const needles=['/api/ticket-market/book/','/api/tickets','Authorization','Bearer ','localStorage.setItem','localStorage.getItem','MY TICKETS','SELL TO BID','renderExchange','renderTicket','ticketMarket'];
-for(const n of needles){let i=0,count=0;console.log('\n===',n,'===');while((i=s.indexOf(n,i))>=0&&count<8){console.log(s.slice(Math.max(0,i-500),Math.min(s.length,i+900)).replace(/\s+/g,' '));i+=n.length;count++;}}
