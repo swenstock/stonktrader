8188c4a53bb3ed6372c76bd8fb5c805bcf20b4c4
swenstock <swenstock@gmail.com>
2026-08-19 04:33:56 -0600

    Stage 46 disable duplicate Stage44 chart handlers

---
+++ public/v45-desktop-stage46-v51-pre.js
@@ -0,0 +1,6 @@
+(()=>{
+'use strict';
+if(!window.matchMedia('(min-width:901px)').matches)return;
+// Stage 46 prevents the retired Stage 44 interaction engine from binding duplicate wheel/pan handlers.
+window.__sbcDesktopStage44V49=true;
+})();
