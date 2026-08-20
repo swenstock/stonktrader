b4be746e07300f55d7e39c8ab85ea09e08a71066
swenstock <swenstock@gmail.com>
2026-08-20 07:00:16 -0600

    Stage 63: cache-bust cleaned analytics modal CSS

--- public/v45-desktop-stage46-v51-pre.js
+++ public/v45-desktop-stage46-v51-pre.js
@@ -7,7 +7,7 @@ window.__sbcDesktopStage44V49=true;
 window.__sbcDesktopStage47V52=true;
 window.__sbcDesktopStage48V53=true;
 window.__sbcDesktopStage49V54=true;
-if(!document.querySelector('link[data-sbc-stage51-v55]')){const l=document.createElement('link');l.rel='stylesheet';l.href='/v45-desktop-stage51-v55.css?v=55';l.dataset.sbcStage51V55='1';document.head.appendChild(l);}
+if(!document.querySelector('link[data-sbc-stage51-v55]')){const l=document.createElement('link');l.rel='stylesheet';l.href='/v45-desktop-stage51-v55.css?v=63';l.dataset.sbcStage51V55='1';document.head.appendChild(l);}
 if(!document.querySelector('script[data-sbc-stage51-v55]')){const s=document.createElement('script');s.src='/v45-desktop-stage51-v55.js?v=62';s.dataset.sbcStage51V55='1';s.defer=true;document.head.appendChild(s);}
 // Stage 52 preserves the compact desktop focus/perspective while Stage 51 owns analytics placement.
 if(!document.querySelector('link[data-sbc-stage52-v56]')){const l=document.createElement('link');l.rel='stylesheet';l.href='/v45-desktop-stage52-v56.css?v=56';l.dataset.sbcStage52V56='1';document.head.appendChild(l);}
